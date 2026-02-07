3a29db54b59a474b1cb7e0e0f0b2d189d3e9eb1c
DTSmth <ducktapedave85@gmail.com>
2026-02-07 08:59:57 -0500

    Init commit Recrating my Care Connect webapp with a Rust backend

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use axum::{routing::get, Json, Router};
+use sqlx::postgres::PgPoolOptions;
+use std::net::SocketAddr;
+use tower_http::cors::CorsLayer;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Status {
+    status: String,
+    db_connected: bool,
+}
+
+#[tokio::main]
+async fn main() {
+    // Load .env and connect to postgres
+    dotenvy::dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .expect("Failed to connect to Postgres");
+
+    //Change for prod
+    let cors = CorsLayer::permissive();
+
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/health", get(health_check))
+        .layer(cors)
+        .with_state(pool);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Server started at{}", addr);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Welcome to Care Rust "
+}
+
+async fn health_check() -> Json<Status> {
+    Json(Status {
+        status: "Up".to_string(),
+        db_connected: false,
+    })
+}
